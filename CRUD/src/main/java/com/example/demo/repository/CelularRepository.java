f9c82b8a322dd39427c713d27477105a649ded44
Mylena <mylenapof@hotmail.com>
2025-05-10 16:40:35 -0300

    Delete CRUD directory

--- CRUD/src/main/java/com/example/demo/repository/CelularRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Celular;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CelularRepository extends JpaRepository<Celular, Long> {
-}
